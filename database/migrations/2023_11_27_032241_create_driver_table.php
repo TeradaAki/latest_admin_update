09b82330d2785cefcae8f53c4ba62a4c00086c08
Aki Terada <terada.aki@auf.edu.ph>
2023-11-27 12:27:56 +0800

    first commit

---
+++ database/migrations/2023_11_27_032241_create_driver_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('driver')) {
+            Schema::create('driver', function (Blueprint $table) {
+                $table->id('driver_id');
+                $table->string('email')->unique();
+                $table->string('first_name');
+                $table->string('last_name');
+                $table->integer('phone_number')->unique();
+                $table->datetime('date_of_birth');
+                $table->integer('bank_account');
+                $table->string('bank_holder');
+                $table->string('vehicle_Type');
+                $table->string('driver_license_num');
+                $table->string('password');
+                $table->integer('earnings');
+                $table->binary('license');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('driver');
+    }
+};
---
+++ resources/views/dashboard/allorder.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>testing</h1>
+</body>
+</html>
+
+
+
+
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>All Order</title>
+    <link rel="stylesheet" href="{{ asset('css/list.css') }}">
+</head>
+<body>
+    @extends('layout.header')
+    @extends('layout.sidenav')
+
+    @section('add_content')
+    <div class="list_table">
+        <div class="list_header">
+            <h2>Drivers</h2>
+        </div>
+        <form class="search_form" action="">
+            <input class="search_bar" type="text" placeholder="Search.." name="search">
+            <button class="search_btn" type="submit">
+                <i class="bi bi-search"></i>
+            </button>
+        </form>
+        <hr>
+        <div class="none">
+            <p class="none_message">No orders found.</p>
+        </div>
+    </div>
+    @endsection
+</body>
+</html> --}}
